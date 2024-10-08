6cba69e0679e0d2210689b02d6b6dfc9cfa8549c
patjoey <pjoh0018@student.monash.edu>
2024-10-08 17:58:38 +1100

    Update visMap.js

--- visMap.js
+++ visMap.js
@@ -1,4 +1,4 @@
-var map = "map.vg.json";
+var map = "https://raw.githubusercontent.com/patjoey/vis2/refs/heads/main/map.vg.json";
 vegaEmbed("#map", map).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);

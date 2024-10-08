2d109cf36c117290d174563fe451901e9c4129ad
patjoey <pjoh0018@student.monash.edu>
2024-10-08 17:48:00 +1100

    Add files via upload

---
+++ area.js
@@ -0,0 +1,4 @@
+var area = "area.vg.json";
+vegaEmbed("#area", area).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
---
+++ visScatter.js
@@ -0,0 +1,4 @@
+var scat = "scatter.vg.json";
+vegaEmbed("#scat", scat).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

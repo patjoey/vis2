59e7d678928ded0663a526520be0a454012a5e2c
patjoey <pjoh0018@student.monash.edu>
2024-10-13 21:43:00 +1100

    Add files via upload

--- visScatter.js
+++ visScatter.js
@@ -1,4 +1,4 @@
 var scat = "scatter.vg.json";
-vegaEmbed("#scat", scat).then(function(result) {
+vegaEmbed("#scat", scat, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);

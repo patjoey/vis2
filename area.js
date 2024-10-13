5bc40eb50e64bfc9e216722b30e636a153b13b50
patjoey <pjoh0018@student.monash.edu>
2024-10-13 21:38:33 +1100

    Add files via upload

--- area.js
+++ area.js
@@ -1,4 +1,4 @@
 var area = "area.vg.json";
-vegaEmbed("#area", area,{"actions": false}).then(function(result) {
+vegaEmbed("#area", area).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
--- stream.js
+++ stream.js
@@ -1,4 +1,4 @@
-var stream = "stream.vg.json";
-vegaEmbed("#stream", stream,{"actions": false}).then(function(result) {
+var stream = "https://raw.githubusercontent.com/patjoey/vis2/refs/heads/main/stream.vg.json";
+vegaEmbed("#stream", stream).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
